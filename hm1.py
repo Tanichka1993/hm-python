157db09876bcf5f0e37ba88443a099b76d5b3639
tanya <tanyamakar1993@gmail.com>
2018-03-14 22:34:12 +0200

    Add version with "format"

--- hm1.py
+++ hm1.py
@@ -1,5 +1,7 @@
 # MESSAGE = 'I am %s %s, I am studying at the %s with %s, my average mark is %f'
-MESSAGE = 'I am {} {}, I am studying at the {} with {}, my average mark is {}'
+# MESSAGE = 'I am {} {}, I am studying at the {} with {}, my average mark is {}'
+MESSAGE = 'I am {name} {surname}, I am studying at the {university} with {group_name}, ' \
+          'my average mark is {average_mark}'
 
 name = input("Enter your name: ").capitalize()
 surname = input("Enter your surname: ").capitalize()
@@ -8,4 +10,11 @@ university = input("Enter your name of university: ").upper()
 average_mark = float(input("Enter your average mark : "))
 
 # print(MESSAGE % (name, surname, university, group_name, average_mark))
-print(MESSAGE.format(name, surname, university, group_name, average_mark))
+# print(MESSAGE.format(name, surname, university, group_name, average_mark))
+print(MESSAGE.format(
+    name=name,
+    surname=surname,
+    university=university,
+    group_name=group_name,
+    average_mark=average_mark
+))
